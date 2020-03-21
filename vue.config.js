7242768ccb3d3acfff99fe6b3cffee34174c5beb
zqy159 <13097073991@163.com>
2020-03-21 14:12:32 +0800

    撒旦发生

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
             },
         },
     },
-    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+    publicPath: './',
     /* 输出文件目录：在npm run build时，生成文件的目录名称 */
     // outputDir: 'dist',
     // /* 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录 */
