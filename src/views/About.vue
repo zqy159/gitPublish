60b3bb88673f21439d37d79271e195aa7ce5c980
zqy159 <13097073991@163.com>
2020-03-24 21:55:25 +0800

    撒旦撒旦

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    aboutassssssss
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'about',
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Home">
+    Home
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
